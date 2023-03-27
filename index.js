246a1c459b4e414c8ae8ea8fa1492e682768498f
Sahil pillania <sahilpillania88@gmail.com>
2023-03-27 22:46:21 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 require("dotenv").config();
 const main = require("./db");
 main();
