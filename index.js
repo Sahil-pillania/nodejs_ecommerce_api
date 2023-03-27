3ed3d6f64146acf718b58968c1ae5ba9e7bf3c1d
Sahil pillania <sahilpillania88@gmail.com>
2023-03-27 22:19:08 +0530

    setup file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 5000;
+require("dotenv").config();
+const main = require("./db");
+main();
+
+// body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routes
+app.use("/", require("./Routes/apiRoutes.js"));
+
+// port
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
